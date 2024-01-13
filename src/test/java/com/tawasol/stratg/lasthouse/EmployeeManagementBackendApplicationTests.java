291223c0b28c8216ba9612f8d48b1eec3d7c47cd
ankith255 <ankith4java@gmail.com>
2024-01-13 22:23:45 +0530

    base project

---
+++ src/test/java/com/tawasol/stratg/lasthouse/EmployeeManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawasol.stratg.lasthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
